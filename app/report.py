35cfe75633508a8b59fc26f6f9b83e306a862e8d
Bruno <bruno.ariev@gmail.com>
2019-06-12 17:57:57 -0300

    Generating file body

--- app/report.py
+++ app/report.py
@@ -7,7 +7,7 @@ def generate_report(env, voltage_ts, filename):
 
     file = open(filepath, 'w')
     create_file_header(voltage_ts, file, env)
-
+    create_file_body(voltage_ts, file, env)
     file.close()
 
 
@@ -18,8 +18,18 @@ def create_folder_structure(filepath):
 
 
 def create_file_header(voltage_ts, file, env):
-    file.write('# Time (ms);')
+    file.write('# Time (ms)')
     for col in range(0, len(voltage_ts[0])):
-        file.write("Voltage(")
+        file.write(";Voltage(")
         file.write(str(env.space_step * col))
-        file.write(", T);")
+        file.write(", T)")
+    file.write("\n")
+
+
+def create_file_body(voltage_ts, file, env):
+    for line in range(0, len(voltage_ts)):
+        file.write(str(env.time_step * line))
+        for col in range(0, len(voltage_ts[0])):
+            file.write(";")
+            file.write(str(voltage_ts[line][col]))
+        file.write("\n")
